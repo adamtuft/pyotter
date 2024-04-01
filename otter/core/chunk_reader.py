f9c0db742c70e87138b91de88c09df12b25583de
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-01 16:48:36 +0100

    Formatting, add TODO

--- otter/core/chunk_reader.py
+++ otter/core/chunk_reader.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from typing import Protocol, Tuple, Generator, Iterable
-import sqlite3
 
 from otf2_ext.events import EventType
 
@@ -13,39 +12,38 @@ from .chunks import Chunk
 
 class SeekEventsCallback(Protocol):
 
-    def __call__(self, positions: Iterable[Tuple[int, int]], batch_size: int = 100) -> Iterable[Tuple[int, Tuple[int, EventType]]]:
-        ...
-
+    def __call__(
+        self, positions: Iterable[Tuple[int, int]], batch_size: int = 100
+    ) -> Iterable[Tuple[int, Tuple[int, EventType]]]: ...
 
 
 class ChunkReaderProtocol(Protocol):
     """Responsible for reading or re-constructing the chunks of a trace"""
 
-    def __iter__(self) -> Generator[int, None, None]:
-        ...
+    def __iter__(self) -> Generator[int, None, None]: ...
 
-    def __len__(self) -> int:
-        ...
+    def __len__(self) -> int: ...
 
-    def items(self) -> Iterable[Tuple[int, Chunk]]:
-        ...
+    def items(self) -> Iterable[Tuple[int, Chunk]]: ...
 
     @property
-    def chunks(self) -> Iterable[Chunk]:
-        ...
+    def chunks(self) -> Iterable[Chunk]: ...
 
-    def get_chunk(self, key: int) -> Chunk:
-        ...
+    def get_chunk(self, key: int) -> Chunk: ...
 
-    def contains(self, key: int) -> bool:
-        ...
+    def contains(self, key: int) -> bool: ...
 
 
+# TODO: Refactor this into otter.db and update to work without sqlite row factory
 class DBChunkReader:
     """Read chunks from a database"""
 
     def __init__(
-        self, attributes, seek_events: SeekEventsCallback, con: db.Connection, bufsize: int = 100
+        self,
+        attributes,
+        seek_events: SeekEventsCallback,
+        con: db.Connection,
+        bufsize: int = 100,
     ) -> None:
         self.con = con
         self.bufsize = bufsize
@@ -71,7 +69,9 @@ class DBChunkReader:
             yield chunk
 
     def get_chunk(self, key: int) -> Chunk:
-        rows: Iterable[Tuple[int, int]] = self.con.execute(db.scripts.get_chunk_events, (key,)).fetchall()
+        rows: Iterable[Tuple[int, int]] = self.con.execute(
+            db.scripts.get_chunk_events, (key,)
+        ).fetchall()
         chunk = Chunk()
         for pos, (location, event) in self._seek_events(rows):
             chunk.append_event(Event(event, self._attributes))
@@ -80,4 +80,3 @@ class DBChunkReader:
     def contains(self, key: int) -> bool:
         rows = self.con.execute(db.scripts.get_chunk_events, (key,)).fetchall()
         return len(rows) > 0
-    
