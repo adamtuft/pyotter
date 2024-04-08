e718ec15259a56a93b0911e26b0443cbfd9ae84c
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-08 17:13:13 +0100

    Simplify definition of TaskDescriptor and TaskAttributes

--- otter/db/types.py
+++ otter/db/types.py
@@ -35,40 +35,9 @@ class TaskDescriptor:  # For use in Connection.parent_child_attributes
 
     label: str
     flavour: int
-    init_file: InitVar[str]
-    init_func: InitVar[str]
-    init_line: InitVar[int]
-    start_file: InitVar[str]
-    start_func: InitVar[str]
-    start_line: InitVar[int]
-    end_file: InitVar[str]
-    end_func: InitVar[str]
-    end_line: InitVar[int]
-    init_location: SourceLocation = field(init=False)
-    start_location: SourceLocation = field(init=False)
-    end_location: SourceLocation = field(init=False)
-
-    def __post_init__(
-        self,
-        init_file: str,
-        init_func: str,
-        init_line: int,
-        start_file: str,
-        start_func: str,
-        start_line: int,
-        end_file: str,
-        end_func: str,
-        end_line: int,
-    ) -> None:
-        super().__setattr__(
-            "init_location", SourceLocation(init_file, init_func, init_line)
-        )
-        super().__setattr__(
-            "start_location", SourceLocation(start_file, start_func, start_line)
-        )
-        super().__setattr__(
-            "end_location", SourceLocation(end_file, end_func, end_line)
-        )
+    init_location: SourceLocation
+    start_location: SourceLocation
+    end_location: SourceLocation
 
     def is_null(self) -> bool:
         return self.label is None and self.flavour is None
@@ -86,49 +55,31 @@ class TaskAttributes:
     parent: int
     children: int
     flavour: InitVar[int]
-    label: InitVar[str]
     create_ts: str
     start_ts: str
     end_ts: str
-    init_file: InitVar[str]
-    init_func: InitVar[str]
-    init_line: InitVar[int]
-    start_file: InitVar[str]
-    start_func: InitVar[str]
-    start_line: InitVar[int]
-    end_file: InitVar[str]
-    end_func: InitVar[str]
-    end_line: InitVar[int]
+    label: InitVar[str]
+    init_location: InitVar[SourceLocation]
+    start_location: InitVar[SourceLocation]
+    end_location: InitVar[SourceLocation]
     descriptor: TaskDescriptor = field(init=False)
 
     def __post_init__(
         self,
         flavour: int,
         label: str,
-        init_file: str,
-        init_func: str,
-        init_line: int,
-        start_file: str,
-        start_func: str,
-        start_line: int,
-        end_file: str,
-        end_func: str,
-        end_line: int,
+        init_location: SourceLocation,
+        start_location: SourceLocation,
+        end_location: SourceLocation,
     ) -> None:
         super().__setattr__(
             "descriptor",
             TaskDescriptor(
                 label,
                 flavour,
-                init_file,
-                init_func,
-                init_line,
-                start_file,
-                start_func,
-                start_line,
-                end_file,
-                end_func,
-                end_line,
+                init_location,
+                start_location,
+                end_location,
             ),
         )
 
