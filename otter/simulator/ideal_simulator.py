bbbcbe31b605340212b7ed78a68379f1489490c8
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-09 12:46:20 +0100

    Refactor to use tasks instead of IDs since it is now fast to get a task instance from the db

--- otter/simulator/ideal_simulator.py
+++ otter/simulator/ideal_simulator.py
@@ -3,6 +3,8 @@ from itertools import count
 
 import otter.log
 import otter.db
+
+from otter.db.types import Task
 from otter.definitions import TaskAction
 
 
@@ -34,22 +36,14 @@ class TaskScheduler:
     def run(self) -> None:
         max_root_task_dt = 0
         global_ts = 0
-        root_task_attributes = self.con.task_attributes(self._root_tasks)
+        root_task_attributes = self.con.get_tasks(self._root_tasks)
         otter.log.info("simulate %d tasks", self.con.num_tasks())
-        for root_task_attr in root_task_attributes:
-            print(f"Simulate root task {root_task_attr.id}")
-            print(f"    Start: {root_task_attr.descriptor.start_location}")
-            print(f"    End:   {root_task_attr.descriptor.end_location}")
-            duration_observed = int(root_task_attr.end_ts) - int(
-                root_task_attr.start_ts
-            )
-            duration = self.descend(
-                root_task_attr.id,
-                root_task_attr.start_ts,
-                root_task_attr.end_ts,
-                0,
-                global_ts,
-            )
+        for root_task in root_task_attributes:
+            print(f"Simulate root task {root_task.id}")
+            print(f"    Start: {root_task.attr.start_location}")
+            print(f"    End:   {root_task.attr.end_location}")
+            duration_observed = int(root_task.end_ts) - int(root_task.start_ts)
+            duration = self.descend(root_task, 0, global_ts)
             speedup = duration_observed / duration
             max_root_task_dt = max(max_root_task_dt, duration)
             print("    Duration:")
@@ -58,28 +52,19 @@ class TaskScheduler:
             print(f"      speedup:   {speedup:>12.2f}")
             print(f"      relative:  {1/speedup:12.2%}")
 
-    def descend(
-        self, task: int, start_ts: str, end_ts: str, depth: int, global_start_ts: int
-    ):
+    def descend(self, task: Task, depth: int, global_start_ts: int):
         """Descend into the children of task. At the root, handle leaf tasks"""
         # self.schedule_writer.insert task-start action
-        self.schedule_writer.insert(task, TaskAction.START, global_start_ts)
-        if self.con.num_children(task) > 0:
-            duration = self.branch_task(task, start_ts, end_ts, depth, global_start_ts)
+        self.schedule_writer.insert(task.id, TaskAction.START, global_start_ts)
+        if task.children > 0:
+            duration = self.branch_task(task, depth, global_start_ts)
         else:
-            duration = self.leaf_task(task, start_ts, end_ts, depth, global_start_ts)
+            duration = self.leaf_task(task, depth, global_start_ts)
         # self.schedule_writer.insert task-complete action
-        self.schedule_writer.insert(task, TaskAction.END, global_start_ts + duration)
+        self.schedule_writer.insert(task.id, TaskAction.END, global_start_ts + duration)
         return duration
 
-    def branch_task(
-        self,
-        task: int,
-        start_ts: str,
-        end_ts: str,
-        depth: int,
-        global_start_ts: int,
-    ):
+    def branch_task(self, task: Task, depth: int, global_start_ts: int):
         """Returns duration elapsed from task-start to task-end, including time spent
         waiting for children/descendants, but not including the duration of any
         children which are not synchronised
@@ -103,8 +88,8 @@ class TaskScheduler:
 
         execution_native_dt, suspended_ideal_dt = 0, 0
 
-        task_states = self.con.task_scheduling_states((task,))
-        task_suspend_meta = dict(self.con.task_suspend_meta(task))
+        task_states = self.con.task_scheduling_states((task.id,))
+        task_suspend_meta = dict(self.con.task_suspend_meta(task.id))
         children_pending: Dict[str, List[Tuple[int, str]]] = {}
         otter.log.debug("got %d task scheduling states", len(task_states))
         barrier_counter = count()
@@ -114,7 +99,7 @@ class TaskScheduler:
                 execution_native_dt = execution_native_dt + state.duration
                 # store the children created during this period
                 children_pending[state.end_ts] = self.con.children_created_between(
-                    task, state.start_ts, state.end_ts
+                    task.id, state.start_ts, state.end_ts
                 )
                 global_start_ts = global_start_ts + state.duration
             elif state.action_start == TaskAction.SUSPEND:
@@ -126,25 +111,23 @@ class TaskScheduler:
                 children = children_pending.get(state.start_ts, [])
                 child_ids: List[int]
                 child_ids, _ = list(zip(*children))
-                children_attr = self.con.task_attributes(child_ids)
-                for child_attr in children_attr:
-                    child_crt_dt = int(state.start_ts) - int(child_attr.create_ts)
+                children_attr = self.con.get_tasks(child_ids)
+                for child in children_attr:
+                    child_crt_dt = int(state.start_ts) - int(child.create_ts)
                     child_duration = self.descend(
-                        child_attr.id,
-                        child_attr.start_ts,
-                        child_attr.end_ts,
+                        child,
                         depth + 1,
                         global_start_ts - child_crt_dt,
                     )
                     duration_into_barrier = child_duration - (
-                        int(state.start_ts) - int(child_attr.create_ts)
+                        int(state.start_ts) - int(child.create_ts)
                     )
                     if duration_into_barrier > barrier_duration:
                         barrier_duration = duration_into_barrier
-                        critical_task = child_attr.id
+                        critical_task = child.id
                 if critical_task is not None:
                     self.crit_task_writer.insert(
-                        task, next(barrier_counter), critical_task
+                        task.id, next(barrier_counter), critical_task
                     )
                 suspended_ideal_dt = suspended_ideal_dt + barrier_duration
                 global_start_ts = global_start_ts + barrier_duration
@@ -156,18 +139,11 @@ class TaskScheduler:
 
         return execution_native_dt + suspended_ideal_dt
 
-    def leaf_task(
-        self,
-        task: int,
-        start_ts: str,
-        end_ts: str,
-        depth: int,
-        global_start_ts: int,
-    ):
+    def leaf_task(self, task: Task, depth: int, global_start_ts: int):
         # Returns the duration of a leaf task. Assumes a leaf task is executed in one go i.e. never suspended.
         pre = "+" * depth
-        start = int(start_ts)
-        end = int(end_ts)
+        start = int(task.start_ts)
+        end = int(task.end_ts)
         duration = end - start
         return duration
 
