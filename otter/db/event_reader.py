fca55e785abbb501b2999aabd78301dabbcafe69
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-16 15:51:09 +0100

    Store sql query as private class member

--- otter/db/event_reader.py
+++ otter/db/event_reader.py
@@ -10,6 +10,8 @@ from ..core.events import Event
 class EventReader:
     """Read events from a database by seeking specific events in the trace file"""
 
+    _get_task_events = scripts["get_task_events"]
+
     def __init__(
         self,
         attributes,
@@ -22,7 +24,7 @@ class EventReader:
         self._seek_events = seek_events
 
     def get_events(self, task: int):
-        event_positions = self.con.execute(scripts["get_task_events"], (task,))
+        event_positions = self.con.execute(self._get_task_events, (task,))
         return [
             Event(event, self._attributes)
             for pos, (location, event) in self._seek_events(event_positions)
