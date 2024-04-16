e62a45352146cf0ddd74a7e88825490328b4226b
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-16 12:06:00 +0100

    Remove  scripts now handled by BufferedDBWriter class

--- otter/db/_scripts/insert_chunk_events.sql
+++
@@ -1,5 +0,0 @@
-insert into chunk_contents values(
-    ?, -- chunk_key
-    ?, -- location_ref
-    ?  -- event_pos
-);
--- otter/db/_scripts/insert_task_relations.sql
+++
@@ -1,6 +0,0 @@
--- Insert parent-child links
-
-insert into task_relation values(
-    ?, -- parent_id
-    ?  -- child_id
-);
