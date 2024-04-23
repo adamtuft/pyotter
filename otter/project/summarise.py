20297216f62c9b4ea2675c4d14ed98be4ebf706b
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-23 09:15:43 +0100

    Use updated ReadConnection methods and print here.

--- otter/project/summarise.py
+++ otter/project/summarise.py
@@ -12,21 +12,36 @@ def summarise_tasks_db(
 ) -> None:
     """Print summary information about a tasks database"""
 
-    project = ReadTraceData(anchorfile, debug=debug)
+    project = ReadTraceData(anchorfile)
 
-    with project.connection() as con:
+    with project.connect() as con:
 
         if summarise == Summarise.ROWCOUNT:
-            con.print_row_count(sep="\t")
+            for items in con.count_rows():
+                print(*items, sep="\t")
 
         elif summarise == Summarise.SOURCE:
-            con.print_source_locations(sep="\t")
+            for location_id, location in con.get_all_source_locations():
+                print(location_id, location.file, location.line, location.func, sep="\t")
 
         elif summarise == Summarise.STRINGS:
-            con.print_strings(sep="\t")
+            for ref, string in con.get_all_strings():
+                print(ref, string, sep="\t")
 
         elif summarise == Summarise.TASKS:
-            con.print_tasks(sep="\t")
+            for attr, num_tasks in con.iter_all_task_types():
+                print(
+                    num_tasks,
+                    attr.label,
+                    f"{attr.create_location.file}:{attr.create_location.line}",
+                    f"{attr.start_location.file}:{attr.start_location.line}",
+                    f"{attr.end_location.file}:{attr.end_location.line}",
+                    sep="\t",
+                )
+
+        elif summarise == Summarise.SIMS:
+            for sim_id, rows in con.count_simulation_rows():
+                print(sim_id, rows, sep="\t")
 
         else:
             otter.log.error("don't know how to summarise %s", summarise)
