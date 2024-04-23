66401f6a5409e47c9ec05a7459bf8040e16e6a71
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-23 09:25:25 +0100

    Refactor to use new classes and protocols.
    - Remove ScheduleWriterProtocol, replace by using a TaskActionWriter.
    
    - Remove CriticalTaskWriterProtocol, replaced by CriticalTaskCallback.
    
    - TaskScheduler now only needs a ReadConnection and callbacks for
      writing simulated schedule.
    
    - No need to manually handle various writers, this is taken care of
      by the writers to which the callbacks are bound, and managed by
      the write-connection in the calling scope.
    
    - Using callable protocols instead of protocols with named methods
      allows for less coupling.

--- otter/simulator/ideal_simulator.py
+++ otter/simulator/ideal_simulator.py
@@ -4,40 +4,39 @@ from itertools import count
 import otter.log
 import otter.db
 
+from otter.log import Loggable
+from otter.db import ReadConnection
+from otter.db.protocols import TaskActionCallback, TaskSuspendMetaCallback, CriticalTaskCallback
 from otter.db.types import Task
 from otter.definitions import TaskAction
 
 
-class ScheduleWriterProtocol(Protocol):
-
-    def insert(self, task: int, action: TaskAction, event_ts: int, /, *args): ...
-
-
-class CriticalTaskWriterProtocol(Protocol):
-
-    def insert(self, task: int, sequence: int, critical_child: int, /, *args): ...
-
-
-class TaskScheduler:
+class TaskScheduler(Loggable):
 
     def __init__(
         self,
-        con: otter.db.Connection,
-        schedule_writer: ScheduleWriterProtocol,
-        crit_task_writer: CriticalTaskWriterProtocol,
+        con: ReadConnection,
+        crit_task_callback: CriticalTaskCallback,
+        task_action_callback: TaskActionCallback,
+        task_suspend_callback: TaskSuspendMetaCallback,
         initial_tasks: Optional[Sequence[int]] = None,
     ) -> None:
+        self.log_debug("CALLBACKS:")
+        self.log_debug("CALLBACKS: %s", crit_task_callback)
+        self.log_debug("CALLBACKS: %s", task_action_callback)
+        self.log_debug("CALLBACKS: %s", task_suspend_callback)
         self.con = con
-        self.schedule_writer = schedule_writer
-        self.crit_task_writer = crit_task_writer
-        self._root_tasks = initial_tasks or con.root_tasks()
+        self.crit_task_callback = crit_task_callback
+        self.task_action_callback = task_action_callback
+        self.task_suspend_callback = task_suspend_callback
+        self._root_tasks = initial_tasks or con.get_root_tasks()
         otter.log.debug("found %d root tasks", len(self._root_tasks))
 
     def run(self) -> None:
         max_root_task_dt = 0
         global_ts = 0
         root_task_attributes = self.con.get_tasks(self._root_tasks)
-        otter.log.info("simulate %d tasks", self.con.num_tasks())
+        otter.log.info("simulate %d tasks", self.con.count_tasks())
         for root_task in root_task_attributes:
             print(f"Simulate root task {root_task.id}")
             print(f"    Start: {root_task.attr.start_location}")
@@ -54,14 +53,16 @@ class TaskScheduler:
 
     def descend(self, task: Task, depth: int, global_start_ts: int):
         """Descend into the children of task. At the root, handle leaf tasks"""
-        # self.schedule_writer.insert task-start action
-        self.schedule_writer.insert(task.id, TaskAction.START, global_start_ts)
+        self.task_action_callback(
+            task.id, TaskAction.START, str(global_start_ts), task.attr.start_location
+        )
         if task.children > 0:
             duration = self.branch_task(task, depth, global_start_ts)
         else:
             duration = self.leaf_task(task, depth, global_start_ts)
-        # self.schedule_writer.insert task-complete action
-        self.schedule_writer.insert(task.id, TaskAction.END, global_start_ts + duration)
+        self.task_action_callback(
+            task.id, TaskAction.END, str(global_start_ts + duration), task.attr.end_location
+        )
         return duration
 
     def branch_task(self, task: Task, depth: int, global_start_ts: int):
@@ -88,8 +89,8 @@ class TaskScheduler:
 
         execution_native_dt, suspended_ideal_dt = 0, 0
 
-        task_states = self.con.task_scheduling_states((task.id,))
-        task_suspend_meta = dict(self.con.task_suspend_meta(task.id))
+        task_states = self.con.get_task_scheduling_states((task.id,))
+        task_suspend_meta = dict(self.con.get_task_suspend_meta(task.id))
         children_pending: Dict[str, List[Tuple[int, str]]] = {}
         otter.log.debug("got %d task scheduling states", len(task_states))
         barrier_counter = count()
@@ -98,15 +99,19 @@ class TaskScheduler:
                 # task in an active state
                 execution_native_dt = execution_native_dt + state.duration
                 # store the children created during this period
-                children_pending[state.end_ts] = self.con.children_created_between(
+                children_pending[state.end_ts] = self.con.get_children_created_between(
                     task.id, state.start_ts, state.end_ts
                 )
                 global_start_ts = global_start_ts + state.duration
             elif state.action_start == TaskAction.SUSPEND:
                 # task is suspended
+                self.task_action_callback(
+                    task.id, TaskAction.SUSPEND, str(global_start_ts), state.start_location
+                )
                 critical_task = None
                 barrier_duration = 0
                 sync_descendants = task_suspend_meta[state.start_ts]
+                self.task_suspend_callback(task.id, state.start_ts, sync_descendants)
                 # get the children synchronised at this point
                 children = children_pending.get(state.start_ts, [])
                 child_ids: List[int]
@@ -126,11 +131,12 @@ class TaskScheduler:
                         barrier_duration = duration_into_barrier
                         critical_task = child.id
                 if critical_task is not None:
-                    self.crit_task_writer.insert(
-                        task.id, next(barrier_counter), critical_task
-                    )
+                    self.crit_task_callback(task.id, next(barrier_counter), critical_task)
                 suspended_ideal_dt = suspended_ideal_dt + barrier_duration
                 global_start_ts = global_start_ts + barrier_duration
+                self.task_action_callback(
+                    task.id, TaskAction.RESUME, str(global_start_ts), state.end_location
+                )
             elif state.action_start == TaskAction.CREATE:
                 # task is created and not yet started
                 pass
@@ -148,10 +154,10 @@ class TaskScheduler:
         return duration
 
 
-def simulate_ideal(con: otter.db.Connection):
-    schedule_writer = otter.db.ScheduleWriter(con)
-    crit_task_writer = otter.db.CritTaskWriter(con)
-    con.on_close(schedule_writer.close)
-    con.on_close(crit_task_writer.close)
-    scheduler = TaskScheduler(con, schedule_writer, crit_task_writer)
-    scheduler.run()
+def simulate_ideal(
+    reader: ReadConnection,
+    crit_task_callback: CriticalTaskCallback,
+    task_action_callback: TaskActionCallback,
+    task_suspend_callback: TaskSuspendMetaCallback,
+):
+    TaskScheduler(reader, crit_task_callback, task_action_callback, task_suspend_callback).run()
